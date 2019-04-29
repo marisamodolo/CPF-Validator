a041510649a90094e83a2a5f04fb108e53b70ff1
Laboratória <ma-ah02@live.com>
2019-04-29 12:38:12 -0300

    implementação de testes

--- test/cpf.spec.js
+++ test/cpf.spec.js
@@ -2,11 +2,26 @@ let chai = require("chai");
 let expect = chai.expect;
 let cpf = require("../lib/index.js");
 
-describe("primeiro digito igual ao resultado da conta", () => {
-    it("primeiro digito deve retornar 6", () => {
-        expect(cpf.cpfValidator("47154849860")).to.equal("6")
-    })
-    it("segundo digito deve retornar 0", () => {
-        expect(cpf.cpfValidator("47154849860")).to.equal("0")
+describe("Retornar true para CPF válido", () => {
+    it("Deve retornar true para um CPF válido", () => {
+        expect(cpf.cpfValidator(47154849860)).to.equal(true);
     })
 }) 
+
+describe("Retornar false para CPF inválidos", () => {
+    it("Deve retornar false para um CPF inválido", () => {
+        expect(cpf.cpfValidator(12345678910)).to.equal(false);
+    })
+    it("Deve retornar false para um CPF com todos os números repetidos", () => {
+        expect(cpf.cpfValidator(11111111111)).to.equal(false);
+    })
+    it("Deve retornar false para um CPF com menos de 11 dígitos", () => {
+        expect(cpf.cpfValidator(11111)).to.equal(false);
+    })
+    it("Deve retornar false para um CPF com mais de 11 dígitos", () => {
+        expect(cpf.cpfValidator(1234567891011121314)).to.equal(false);
+    })
+    it("Deve retornar false se informado letras", () => {
+        expect(cpf.cpfValidator("abcdefghijklmn")).to.equal(false);
+    })
+})
